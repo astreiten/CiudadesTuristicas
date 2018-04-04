f0acd1a626dac9ff1d95bb72759929e3ae01b28e
BiancaArtola <bian.artola@hotmail.com>
2018-04-04 11:41:58 -0300

    LO PUSE EN MINISCULA

--- bootstrap/js/funciones.js
+++ bootstrap/js/funciones.js
@@ -5,7 +5,7 @@ function prueba() {
 function init(){
  document.getElementById("campo").firstChild.data = "hola";
  var xmlhttp= new XMLHttpRequest();
- var url='./js/pizzas.JSON';
+ var url='./js/pizzas.json';
 
 
  xmlhttp.onreadystatechange = function() {
