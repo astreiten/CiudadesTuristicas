adb0742fc44d39a27e86fc1fd1b567e391cac137
Agustin Streitenberger <a_streiten@outlook.com>
2018-04-04 10:49:22 -0300

    2

--- js/funciones.js
+++ js/funciones.js
@@ -5,7 +5,7 @@ function prueba() {
 function init(){
  document.getElementById("campo").firstChild.data = "hola";
  var xmlhttp= new XMLHttpRequest();
- var url="http://localhost:/path/to/model";
+ var url="https://github.com/astreiten/CiudadesTuristicas/blob/master/data/pizzas.json";
  xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
@@ -13,7 +13,7 @@ function init(){
     }
     else{
          document.getElementById("campo").firstChild.data = this.status + "aca " + this.readyState;
-         alert("ERROR");
+         
     }
 }
 
