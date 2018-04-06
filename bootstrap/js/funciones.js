324d0586dcf9c9a30bfc59f4ca274887e38cc4a3
BiancaArtola <bian.artola@hotmail.com>
2018-04-06 14:16:11 -0300

    toma mi banner divino hermoso

--- bootstrap/js/funciones.js
+++ bootstrap/js/funciones.js
@@ -41,7 +41,7 @@ function initMap() {
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById("campo"), {
           center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          zoom: 10
         });
       }
 
