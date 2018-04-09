06ba7109fbf915d604cd44f9199aee582be421bd
BiancaArtola <bian.artola@hotmail.com>
2018-04-09 13:48:45 -0300

    prueba

--- bootstrap/js/funcionesRecos.js
+++ bootstrap/js/funcionesRecos.js
@@ -4,6 +4,23 @@ $(function() {
 
 function oyentePaginaRecorrido(){
 	var texto= document.getElementById("paginaRecorrido").value;
-	alert(texto);
+	alert(texto+"djs");
+	alert("kekeke");
 	localStorage.setItem("comentario",texto);
+
+	obtenerInformacionJSON();
+}
+
+function obtenerInformacionJSON(){
+	alert("puto");
+  var xmlhttp = new XMLHttpRequest();
+  var url="https://astreiten.github.io/CiudadesTuristicas/bootstrap/js/recorridos.json";
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+
+        alert(myArr.recorridos[2].nombre);
+
+    }
+        
 }
