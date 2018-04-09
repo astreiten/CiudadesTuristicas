4f8ca1eeb3088ff02248746b46b67f378a22fa28
Agustin Streitenberger <a_streiten@outlook.com>
2018-04-09 13:25:01 -0300

    requerimientos no funcionales de la pesada de blanca

--- bootstrap/js/funcionesRecos.js
+++ bootstrap/js/funcionesRecos.js
@@ -1,12 +1,8 @@
 $(function() {
-  //alert("funciona");
   document.getElementById("eti").firstChild.data = localStorage.getItem("comentario");
 });
 
 function oyentePaginaRecorrido(){
-	
-	alert("hola");
-	//document.getElementById("exa").value="prueba";
 	var texto= document.getElementById("paginaRecorrido").value;
 	alert(texto);
 	localStorage.setItem("comentario",texto);
