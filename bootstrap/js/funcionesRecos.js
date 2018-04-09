8318dc2d83c44045960c18d341d4c85d026f4f5a
BiancaArtola <bian.artola@hotmail.com>
2018-04-09 14:23:59 -0300

    okkkkkkkkkkkkkkk

--- bootstrap/js/funcionesRecos.js
+++ bootstrap/js/funcionesRecos.js
@@ -8,20 +8,25 @@ function oyentePaginaRecorrido(){
 	alert(texto);
 	localStorage.setItem("comentario",texto);
 
-	//obtenerInformacionJSON();
+	obtenerInformacionJSON();
 }
 
 function obtenerInformacionJSON(){
-	alert("puto");
+	alert("pq no entras");
   var xmlhttp = new XMLHttpRequest();
   var url="https://astreiten.github.io/CiudadesTuristicas/bootstrap/js/recorridos.json";
+  	alert("pq no entras2");
   xmlhttp.onreadystatechange = function() {
+  		alert("pq no entras3");
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
+        var nombre = myArr.recorridos[2].nombre;
 
-        alert(myArr.recorridos[2].nombre);
-
-    }
+    }else
+    	alert("state: "+this.readyState+" status: "+this.status);
         
 	}
+
+	  xmlhttp.open("GET", url, true);
+	  xmlhttp.send();
 }
