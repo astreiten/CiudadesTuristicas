ab43318a7a93030bb321a122d8e6866dedb82b1e
BiancaArtola <bian.artola@hotmail.com>
2018-04-10 15:19:57 -0300

    descripcion recorridos

--- bootstrap/js/funcionesRecos.js
+++ bootstrap/js/funcionesRecos.js
@@ -46,6 +46,10 @@ function obtenerDatosRecorridos(myArr){
 	var tiempo = myArr.recorridos[numeroRecorrido].tiempo;
 	document.getElementById("tiempo_recorrido").innerHTML = document.getElementById("tiempo_recorrido").innerHTML +" "+tiempo+" horas";
 
+	//Obtengo descripcion
+	var descripcion = myArr.recorridos[numeroRecorrido].puntos[i].descripcion;
+	document.getElementById("descripcion_recorrido"+i).innerHTML = document.getElementById("descripcion_recorrido").innerHTML +" "+descripcion;
+
 	//Obtengo los puntos
 	for (var i =0 ; i < myArr.recorridos[numeroRecorrido].puntos.length; i++)
 		obtenerPuntos(myArr, i);
